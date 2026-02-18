fad82eee6f544a49bdcd91edc9ff8f73ebfd4e31
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-02-18 19:26:40 +0530

    Force secure cookie for production

--- controllers/seller.controller.js
+++ controllers/seller.controller.js
@@ -25,10 +25,8 @@ export const sellerLogin = async (req, res) => {
             res.status(200).cookie("sellerToken", token,
                 {
                     httpOnly: true,
-                    // secure: process.env.NODE_ENV === "production",
-                    // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-                    secure: process.env.NODE_ENV === "production",
-                    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+                    secure: true, 
+                    sameSite: "none",
 
                     maxAge: 7 * 24 * 60 * 60 * 1000,
                 }).json({
@@ -45,8 +43,8 @@ export const sellerLogout = async (req, res) => {
     try {
         res.clearCookie("sellerToken", {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: true,
+            sameSite: "none",
         });
         res.status(200).json({ message: "User Logout successfully", success: true });
     } catch (error) {
